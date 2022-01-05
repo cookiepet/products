8a2851a1e32ac576af7b3b8d6415530de8fa445b
Peter Pan <dicken0329@gmail.com>
2022-01-05 17:10:29 +0800

    final version

--- products.py
+++ products.py
@@ -5,6 +5,7 @@ while True:
 	if name == 'q': # input 'q' for quit 
 		break
 	price = input('Please input product price:')
+	price = int(price) # Transfer price type from string to integer
 	p = []
 	p.append(name)
 	p.append(price) # equal to p = [name, price]
@@ -13,3 +14,14 @@ print(products)
 
 for p in products:
 	print('Product:', p[0],'price is', p[1] )
+
+# String operation
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+with open('products.csv', 'w', encoding='utf-8') as f: # Just open file. # utf-8 is general coding type.
+	f.write('Producsts name,price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') #\n change line
+
+# 'with' will automatically close the file once the program run out of with.
