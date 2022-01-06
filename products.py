45a9f8e251b139fbfad1d4b6ba482efc5b6275fe
Peter Pan <dicken0329@gmail.com>
2022-01-06 11:33:23 +0800

    Final version

--- products.py
+++ products.py
@@ -1,5 +1,15 @@
-# Two dimensional array
+# Read previous records
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'Products name,price' in line:
+			continue # skip below process then continue in next loop. (Equal to break one times.)
+		name, price = line.strip().split(',') # Strip will remove change line mark (\n).
+		products.append([name, price])
+print(products)
+
+# User input area
+# Two dimensional array
 while True:
 	name = input('Please input products name:')
 	if name == 'q': # input 'q' for quit 
@@ -12,6 +22,7 @@ while True:
 	products.append(p) # equal to products.append([name, price])
 print(products)
 
+# Print out all records.
 for p in products:
 	print('Product:', p[0],'price is', p[1] )
 
@@ -19,8 +30,8 @@ for p in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
-with open('products.csv', 'w', encoding='utf-8') as f: # Just open file. # utf-8 is general coding type.
-	f.write('Producsts name,price\n')
+with open('products.csv', 'w', encoding = 'utf-8') as f: # Just open file. # utf-8 is general coding type.
+	f.write('Products name,price\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n') #\n change line
 
